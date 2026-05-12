6855313866f600e13a3569813166d6f8c5f21abd
christina <chrxssychenn@gmail.com>
2026-05-12 18:24:25 +1200

    WGC Arcade Prize Manager.py
    Everything stayed the same, but I switched them around to match the two functions I added, display_prizes() and menu().
    
    Added comments for ljust() and an example

--- WGC Arcade Prize Manager.py
+++ WGC Arcade Prize Manager.py
@@ -3,36 +3,60 @@
 # Christina C
 
 """WGC Arcade Prize Manager."""
+# List of all prizes
 prizes = [
     {"Name": "Teddy Bear", "Ticket Cost": 160, "Current Stock": 40},
     {"Name": "Fidget Toy", "Ticket Cost": 40, "Current Stock": 50},
     {"Name": "Bouncy Ball", "Ticket Cost": 5, "Current Stock": 100},
     {"Name": "Chocolate", "Ticket Cost": 45, "Current Stock": 75},
     {"Name": "Keychain", "Ticket Cost": 20, "Current Stock": 60},
-    ]
+]
 
-while True:
-    print("\n1. Prizes")
-    print("2. Exit")
-    choice = input("Pick 1 or 2: ")
 
-    if choice == "1":
-        print("\nPRIZES:")
-        print(" | ".join(["Name".ljust(12), "Cost".ljust(4), "Stock"]))
-        print("-" * 30)
+def display_prizes():
+    """Display all the prizes."""
+    print("\nPRIZES:")
+    # ljust() is used to left justify a string within a specified width
+    # example:
+    # fruit = "apple"
+    # x = fruit.ljust(10)
+    # print(x, "is my favorite fruit.")
+    #
+    # output:
+    # apple      is my favorite fruit.
 
-        for prize in prizes:
-            row = [
-                prize["Name"].ljust(12),
-                str(prize["Ticket Cost"]).ljust(4),
-                str(prize["Current Stock"])
-            ]
+    print(" | ".join(["Name".ljust(12), "Cost".ljust(6), "Stock"]))
+    print("-" * 32)
 
-            print(" | ".join(row))
+    for prize in prizes:
 
-    elif choice == "2":
-        print("Goodbye")
-        break
+        row = [
+            prize["Name"].ljust(12),
+            str(prize["Ticket Cost"]).ljust(6),
+            str(prize["Current Stock"])
+        ]
 
-    else:
-        print("This is a invalid choice. Please pick either 1 or 2")
+        print(" | ".join(row))
+
+
+def menu():
+    """Display menu screen."""
+    while True:
+
+        print("\n1. Prizes")
+        print("2. Exit")
+
+        choice = input("Pick 1 or 2: ")
+
+        if choice == "1":
+            display_prizes()
+
+        elif choice == "2":
+            print("Goodbye")
+            break
+
+        else:
+            print("This is an invalid choice. Please pick either 1 or 2")
+
+
+menu()
