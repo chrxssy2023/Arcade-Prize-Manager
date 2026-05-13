ba80c226e31c5ff6ed28b9a69fab83548450b631
christina <chrxssychenn@gmail.com>
2026-05-13 19:45:08 +1200

    WGC Arcade Prize Manager.py
    Added more comments and examples. Finalising phase 1

--- WGC Arcade Prize Manager.py
+++ WGC Arcade Prize Manager.py
@@ -3,6 +3,8 @@
 # Christina C
 
 """WGC Arcade Prize Manager."""
+
+print("\n--- WGC Arcade Prize Manager ---")
 # List of all prizes
 prizes = [
     {"Name": "Teddy Bear", "Ticket Cost": 160, "Current Stock": 40},
@@ -25,38 +27,46 @@ def display_prizes():
     # output:
     # apple      is my favorite fruit.
 
+    # Create table header with aligned colums using ljust()
     print(" | ".join(["Name".ljust(12), "Cost".ljust(6), "Stock"]))
     print("-" * 32)
 
+    # Loops through each prize in the list
     for prize in prizes:
-
+        # Formats each row so each value lines up neatly
         row = [
             prize["Name"].ljust(12),
+            # Without str() the programme will fail
+            # Convert numvers to strings so ljust() can be used
             str(prize["Ticket Cost"]).ljust(6),
             str(prize["Current Stock"])
         ]
-
         print(" | ".join(row))
 
 
 def menu():
     """Display menu screen."""
+    # while True loop will run forever if condition is always true
+    # Infinite loop keeps menu running until user chooses to exit
     while True:
-
         print("\n1. Prizes")
         print("2. Exit")
 
+        # Get user choice
         choice = input("Pick 1 or 2: ")
 
+        # Displays prize list if user selects 1
         if choice == "1":
             display_prizes()
 
+        # Stop/exits the loop of the program if user selects 2
         elif choice == "2":
             print("Goodbye")
             break
 
+        # Handles invalid input
         else:
             print("This is an invalid choice. Please pick either 1 or 2")
 
-
+# Start of the program
 menu()
