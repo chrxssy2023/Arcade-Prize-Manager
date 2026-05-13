73f3c74851ca6c7e3e14c756e4c20bef93c1b547
christina <chrxssychenn@gmail.com>
2026-05-13 21:02:58 +1200

    wgc_arcade_prize_manager.py
    Added and worked on comments

--- wgc_arcade_prize_manager.py
+++ wgc_arcade_prize_manager.py
@@ -18,18 +18,24 @@ prizes = [
 def display_prizes():
     """Display all the prizes."""
     print("\nPRIZES:")
-    # Create table header with aligned colums using ljust()
+
+    # Create table header with aligned columns using ljust()
     print(" | ".join(["Name".ljust(12), "Cost".ljust(6), "Stock"]))
+
+    # Prints a divider line under the headings
     print("-" * 32)
 
     # Loops through each prize in the list
     for prize in prizes:
+
         # Formats each row so each value lines up neatly
         row = [
             prize["Name"].ljust(12),
             str(prize["Ticket Cost"]).ljust(6),
             str(prize["Current Stock"])
         ]
+
+        # Print the formatted row with "|" separating each column
         print(" | ".join(row))
 
 
@@ -38,6 +44,8 @@ def menu():
     # while True loop will run forever if condition is always true
     # Infinite loop keeps menu running until user chooses to exit
     while True:
+
+        # Display menu options
         print("\n1. Prizes")
         print("2. Exit")
 
@@ -53,9 +61,10 @@ def menu():
             print("Goodbye")
             break
 
-        # Handles invalid input
+        # Runs and handles invalid input
         else:
             print("This is an invalid choice. Please pick either 1 or 2")
+
 
 # Start of the program
 menu()
